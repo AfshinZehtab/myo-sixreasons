4169194a1bffbed17c308de416cbdacafb15a93f
AfshinZehtab <azehtab@sixreasons.de>
2016-03-16 13:19:24 +0100

    Added files via upload

--- myo-sixreasons.js
+++ myo-sixreasons.js
@@ -1,14 +1,43 @@
 var Myo = require("myo");
-Myo.connect("com.sixreasons.myo-armband");
+Myo.connect("com.sixreasons.myo-sixreasons");
 
 
-Myo.on("connected", function(data, timestamp) {
-  console.log("Myo successfully connected. Data: " + JSON.stringify(data) + ". Timestamp: " + timestamp + ".");
-});
+// Myo.on('pose', function(pose_name){
+//     console.log('Started ', pose_name);
+// });
+// var myMyo = Myo.create();
+// myMyo.on('wave_in', function(edge){
+//     Menu.left()
+// })
 
-Myo.on("pose", function(pose_name) {
-  console.log("Myo pose detected! Pose was: " + pose_name + ".");
-});
+// Myo.on("connected", function(data, timestamp) {
+//   console.log("Myo successfully connected. Data: " + JSON.stringify(data) + ". Timestamp: " + timestamp + ".");
+
+// });
+
+// Myo.on("pose", function(pose_name) {
+//   console.log("Myo pose detected! Pose was: " + pose_name + ".");
+// });
+
+// Myo.on('gyroscope', function(data) {  
+// 	if (data.x > 100){
+// 	console.log("X");
+// 	}
+// 	else if (data.y > 100){
+// 	console.log("Y");
+// 	}
+// 	else if (data.z > 100){
+// 	console.log("Z");
+// 	}
+// });
+
+
+// Myo.connect('com.stolksdorf.myAwesomeApp');
+
+// Myo.on('fist', function(){
+// 	console.log('Hello Myo!');
+// 	this.vibrate();
+// });
 
 // Myo.on("fingers_spread", function() {
 //   console.log("Fingers spread!");
